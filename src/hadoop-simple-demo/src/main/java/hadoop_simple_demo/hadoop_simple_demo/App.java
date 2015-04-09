06f0340c31e1e1c962c00359965f79daa9db3aaf
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2015-04-09 15:32:54 +0100

    Basic project creation

---
+++ src/hadoop-simple-demo/src/main/java/hadoop_simple_demo/hadoop_simple_demo/App.java
@@ -0,0 +1,13 @@
+package hadoop_simple_demo.hadoop_simple_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
